ba8ca5aeed1652637b43e3a48bee4ab0b21fbd40
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-03-04 22:00:39 +0200

    Task 1 #2

--- Task 1/NLP Task 1.py
+++ Task 1/NLP Task 1.py
@@ -1,10 +1,48 @@
 from nltk.tokenize import *
 
-text = """
-Hi! My name is Abdo Sharaf. I'm a CS student at FACI - DU. This is my graduation year.
-Nice to meat you. Have a nice day! I live in Las Vegas.
-"""
+def get_user_input():
+    print("Please enter some text:")
+    text = input()
+    
+    while len(text) < 3:
+        print("Please enter a valid text:")
+        text = input()
+    
+    print("\nChoose one of these options:-")
+    print("1) Tokenized Words.\n2) Tokenized Sentences.\n3) Using \"split\" Function.")
+    choice = int(input())
+    
+    while choice not in [1, 2, 3]:
+        print("Please enter a valid choice!")
+        choice = int(input())
+    
+    return (text, choice)
 
-print(sent_tokenize(text))
-print(word_tokenize(text))
-print(text.split())
+
+def get_tokenize_words(text: str):
+    return word_tokenize(text)
+
+
+def get_tokenize_sents(text: str):
+    return sent_tokenize(text)
+
+
+def get_splitted_text(text: str):
+    return text.split()
+
+
+if __name__ == "__main__":
+    text, choice = get_user_input()
+    
+    output = ""
+    match choice:
+        case 1:
+            output = get_tokenize_words(text)
+        case 2:
+            output = get_tokenize_sents(text)
+        case 3:
+            output = get_splitted_text(text)
+    
+    print("\n\t\t========================================")
+    print(f"Your Output:-\n{output}")
+
